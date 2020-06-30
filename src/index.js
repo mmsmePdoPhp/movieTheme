e9e7fb2e7096f302828593d0966dc13d8c9a8eea
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-07-01 04:07:25 +0430

    popper js is not working yet but included manully

--- src/index.js
+++ src/index.js
@@ -2,35 +2,38 @@ import _ from 'lodash';
 
 // js files
 import printMe from './print.js';
-import jqeury from 'jquery/src/jquery.js'
+import jquery from 'jquery/src/jquery.js';
 
-require ('bootstrap/dist/js/bootstrap');
+// import popperjs from '@popperjs/core/dist/cjs/popper.js'
+// import hi from  ('bootstrap/dist/js/bootstrap.js');
+// console.log(hi)
+
 import './style.css';
 import './style.scss';
 
 import Selena from './images/women.jpg';
-import { bootstrap } from 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/scss/bootstrap.scss';
 
-function component() {
-    const btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+// function component() {
+//     const btn = document.createElement('button');
+//     btn.innerHTML = 'Click me and check the console!';
+//     btn.onclick = printMe;
 
-    const element = document.createElement('div');
-    const paragraph = document.createElement('p');
+//     const element = document.createElement('div');
+//     const paragraph = document.createElement('p');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['سلام چونی برا گیان خوشی شکر  ', 'webpack'], ' ');
-    paragraph.innerText = "سریف, طراحان ایرانی, عناوین سینما و تلویزیون, فونت اردو, فونت تیتر, فونت رایگان, فونت عربی, فونت کردی, فونت متن, فونت مطبوعاتی, فونت معاصر و مدرن, فونت موبایل و‌ اپلیکیشن, فونت وب, نشر رومیزی.";
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Selena;
+//     // Lodash, currently included via a script, is required for this line to work
+//     element.innerHTML = _.join(['سلام چونی برا گیان خوشی شکر  ', 'webpack'], ' ');
+//     paragraph.innerText = "سریف, طراحان ایرانی, عناوین سینما و تلویزیون, فونت اردو, فونت تیتر, فونت رایگان, فونت عربی, فونت کردی, فونت متن, فونت مطبوعاتی, فونت معاصر و مدرن, فونت موبایل و‌ اپلیکیشن, فونت وب, نشر رومیزی.";
+//     // Add the image to our existing div.
+//     const myIcon = new Image();
+//     myIcon.src = Selena;
 
-   element.appendChild(paragraph)
-   element.appendChild(myIcon)
-   element.appendChild(btn)
+//    element.appendChild(paragraph)
+//    element.appendChild(myIcon)
+//    element.appendChild(btn)
 
-    return element;
-  }
+//     return element;
+// }
 
-  document.body.appendChild(component());
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: {
       app: './src/index.js',
       print: './src/print.js',
+      main: './src/style.scss'
     },
     output: {
         filename: '[name].bundle.js',
